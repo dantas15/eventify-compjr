cadfd1e87bb98463e6a146d575a0a2356ac4564a
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-05-11 09:47:17 -0300

    chore(event): add event schemas for zod and mongo

---
+++ packages/api/src/models/Event.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from 'mongoose';
+import { Event as EventType } from '@/types/event';
+
+const eventSchema = new Schema<EventType>({
+  title: { type: String, required: true },
+  description: { type: String, required: false },
+  date: { type: Date, required: true },
+  location: { type: String, required: false },
+  image: { type: String, required: false }
+});
+
+const eventModel = model('Event', eventSchema);
+export { eventModel as Event };
