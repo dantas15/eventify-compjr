d23201792f7c9549275ff29a6861eb2a7bed13c6
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-05-24 14:21:08 -0300

    fix(jwt): remove payload type and add directly to request type

--- packages/api/src/config/jwt.ts
+++ packages/api/src/config/jwt.ts
@@ -2,12 +2,7 @@ import { config } from 'dotenv';
 
 config();
 
-type Payload = {
-  googleId: string;
-  userId: string;
-};
-
 const secret = process.env.JWT_SECRET ?? 'test';
 const expiresIn = process.env.JWT_EXPIRES_IN ?? '1d';
 
-export { secret, expiresIn, Payload as JwtPayload };
+export { secret, expiresIn };
