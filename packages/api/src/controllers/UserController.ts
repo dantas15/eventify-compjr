cf505c727d5cc3afb7f8160f85eba70d9dc9766c
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-05-24 16:54:16 -0300

    chore(users): remove unused try-catch

--- packages/api/src/controllers/UserController.ts
+++ packages/api/src/controllers/UserController.ts
@@ -7,25 +7,16 @@ import { Event } from '@/models/Event';
 export class UserController {
   async me(request: Request, response: Response) {
     const { userData } = request;
-
-    try {
-      const user = await User.findById(userData.userId);
-      return response.json(user);
-    } catch {
-      throw new AppError('User not found');
-    }
+    const user = await User.findById(userData.userId);
+    return response.json(user);
   }
 
   async myEvents(request: Request, response: Response) {
     const { userData } = request;
 
-    try {
-      const events = await Event.find({
-        userId: userData.userId
-      });
-      return response.json(events);
-    } catch {
-      throw new AppError('User events not found');
-    }
+    const events = await Event.find({
+      userId: userData.userId
+    });
+    return response.json(events);
   }
 }
