d662baa9ec752289bf45df1fa7290ec578364820
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-05-31 09:17:46 -0300

    feat(title): add HeadingTitle component
    - two size options at first: big and regular

---
+++ packages/web/src/components/HeadingTitle.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+type Props = {
+  size: "big" | "regular";
+};
+
+// this is exported by default
+withDefaults(defineProps<Props>(), {
+  size: "regular",
+});
+</script>
+
+<template>
+  <div class="headings">
+    <h1 v-if="size === 'big'" class="green"><slot></slot></h1>
+    <h3 v-else-if="size === 'regular'" ><slot></slot></h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.headings h1,
+.headings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .headings h1,
+  .headings h3 {
+    text-align: left;
+  }
+}
+</style>
