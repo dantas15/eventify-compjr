d380f042b8471b270c6fa9bffc508bd2775def41
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-06-16 16:10:37 -0300

    feat(IconFire): add IconFire for fiery styles

---
+++ packages/web/src/components/icons/IconFire.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { PhFire } from '@phosphor-icons/vue';
+</script>
+
+<template>
+  <PhFire :style="{fill: 'url(#gradient)'}">
+    <linearGradient id="gradient" gradientTransform="rotate(45)">
+      <stop offset="0%" stop-color="rgba(161,1,0,1)"/>
+      <stop offset="30%" stop-color="rgba(218,31,5,1)"/>
+      <stop offset="47%" stop-color="rgba(243,60,4,1)"/>
+      <stop offset="71%" stop-color="rgba(254,101,13,1)"/>
+      <stop offset="92%" stop-color="rgba(255,193,31,1)"/>
+      <stop offset="100%" stop-color="rgba(255,247,93,1)"/>
+    </linearGradient>
+  </PhFire>
+</template>
+
+<style scoped>
+
+</style>
