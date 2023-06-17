5ef4c987c5a2db9425866269217c3c68d61b46a9
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-06-17 11:35:55 -0300

    chore(ButtonLink): add ButtonLink component

---
+++ packages/web/src/components/ButtonLink.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+type Props = {
+  href: string;
+  color: "primary" | "regular"
+}
+withDefaults(defineProps<Props>(), {
+  color: "regular"
+});
+</script>
+
+<template>
+ <a
+   class="button"
+   :class="{'regular-button': (color === 'regular'), 'primary-button': color === 'primary'}"
+ >
+  <slot></slot>
+ </a>
+</template>
+
+<style scoped>
+.button {
+  display: inline-block;
+  padding: 8px 16px;
+  text-decoration: none;
+  border-radius: var(--border-radius-small);
+  user-select: none;
+  cursor:pointer;
+
+  color: var(--color-text);
+}
+
+.primary-button {
+  background: var(--vt-c-primary);
+}
+.primary-button:hover {
+  background: var(--vt-c-primary-hover);
+}
+
+.regular-button {
+  background-color: var(--color-background);
+}
+.regular-button:hover {
+  background: var(--color-background-mute)
+}
+</style>
